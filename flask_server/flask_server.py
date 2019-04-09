9944ce61fc581b8f4f4bd64a98663c07dec2a926
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-04-09 08:45:44 +0200

    Add routes

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -18,8 +18,46 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/restaurants/')
 def index():
-    return "Welcome to Restaurant Menus app"
+    return "restaurants list"
+
+
+@app.route('/restaurants/add/')
+def add_restaurant():
+	return "add restaurant"
+
+
+@app.route('/restaurants/<int:restaurant_id>/edit')
+def edit_restaurant(restaurant_id):
+	return "edit restaurant"
+
+
+@app.route('/restaurants/<int:restaurant_id>/delete')
+def delete_restaurant(restaurant_id):
+	return "delete restaurant"
+
+
+@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+def restaurant_menu(restaurant_id):
+	return "restaurant menu"
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/add')
+def add_menu_item(restaurant_id):
+	return "add menu item"
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:item_id>/edit')
+def edit_menu_item(restaurant_id, item_id):
+	return "edit menu item"
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:item_id>/delete')
+def delete_menu_item(restaurant_id, item_id):
+	return "delete menu item"
+
 
 
 if __name__ == '__main__':
